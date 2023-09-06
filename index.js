69890efcbe950a4126129722aa4861ae28ff4707
Pawandasila <pawandasila06@gmail.com>
2023-09-06 21:27:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,142 @@
+
+/* js console api(appication programming interface) */
+
+//  console.log("Hello world");
+//  console.warn("this is warn");
+//  console.error("this is an error");
+
+//  ways to print in js
+//  alert("me");
+//  document.write("this is");
+//  document.write("this is");
+//  console.clear();
+//  console.log("this is",4+6,"another log");
+
+//js variables
+// var number =34;
+// var number2 =78;
+// console.log(number+number2);
+
+// datatypes in datatypes
+// string
+// var str1="thsi is a string";
+// var str5="thsi is a second string";
+// var str2="thsi is a next string";
+//number
+// var num =3;
+// var num3 = 6;
+
+//
+//Object
+// var marks={
+//     ravi :34,
+//     shibham :34,
+//     sumit :99,
+//     pawan : 99.9999
+// }
+
+//booleans
+// var a =true;
+// var b=false;
+
+
+// undefined,null
+// var und = undefined;
+// var und;
+// console.log(und);
+
+// var n =null;
+// console.log(n);
+
+/* there are two types of datatype
+1) primitive --> undefined, null, number,string,boolean,symbol
+2) referrence --> arrays and objects*/
+
+// var arr=[1,2,3,"string",5,5,3];
+// console.log(arr);
+
+// opeators
+// var a=7;
+// var b=7;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+//function in js
+// function avg(a,b){
+//     c=(a+b)/2;
+//     return c;
+// }
+// c1= avg(3,5);
+// c5= avg(3,5);
+// console.log(c1,c5);
+// console.log("this is a string in different color");
+
+//if else
+// var age = 55;
+// if(age>8){
+//     console.log("you are not a kid anymore");
+// }
+// else{
+//     console.log("you are a kid plese focus in your studies");
+// }
+var arr = [1,2,3,4,5,6];
+// console.log(arr);
+// for(var i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// arr.forEach(function(elements){
+//     console.log(elements);
+// })
+let j=0;
+const ac=78;
+// console.log(ac);
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j++;
+// }
+// do{
+//     console.log(arr[j]);
+//     j++
+// }while(j<arr.length)
+
+// for(var i=0;i<arr.length;i++){
+//     if(i==2){
+//         // break;
+//         continue;
+    // }
+    // console.log(arr[i]);
+
+// }
+
+// let myarr=["fan",3434,3.3,"pawan"];
+//array method
+// console.log(myarr.length);
+// myarr.pop();
+// myarr.push("news");
+// myarr.shift();
+// myarr.unshift("fan");
+// console.log(myarr.unshift("fan"))
+
+// console.log(myarr);
+
+//string method in js
+// let mystr = "pawan is a good boy good";
+// console.log(mystr);
+// console.log(mystr.length);
+// console.log(mystr.indexOf("good"));
+// console.log(mystr.lastIndexOf("good"));
+// console.log(mystr.slice(1,3));
+// console.log(mystr.replace("is","good"));
+
+// mydate= new Date();
+// console.log(mydate);
+// console.log(mydate.getTime());
+// console.log(mydate.getFullYear());
+// console.log(mydate.getDay());
+// console.log(mydate.getMinutes());
+
+var click= document.getElementById('click')
+console.log(click);
